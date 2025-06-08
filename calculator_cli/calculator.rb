6ab19acc2ebfd02e94ecaf7b0ef83adfc63399be
Luis X. Aguirre <149963996+lxaguirre@users.noreply.github.com>
2025-06-08 01:03:53 +0000

    calcu

--- calculator_cli/calculator.rb
+++ calculator_cli/calculator.rb
@@ -1,28 +1,11 @@
 # calculator.rb
 
 class calculator
-    attr_accessor :result
-
-    def initialize
-        @result = 0
+    def add(a, b)
+        a + b
     end
 
-    def add(number)
-        @result += number
+    def subtract(a, b)
+        a - b
     end
-
-    def subtract(number)
-        @result -= number
-    end
-
-    def multiply(number)
-        @result *= number
-    end
-
-    def divide(number)
-        if number == 0
-            pp "Error: Cannot divide by zero."
-        else
-            @result /= number
-        end
-    end
+end
